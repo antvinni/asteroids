58230c4313dc1830b06b84ede1cb8cd522fbc5fe
antvinni <ant.vinni@gmail.com>
2024-09-05 15:40:16 +0100

    recreated venv from vscode

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,8 @@
 import pygame
+import random
+
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +15,11 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt 
         return super().update(dt)
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+            pass ####??????
