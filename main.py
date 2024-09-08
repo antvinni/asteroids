bcd3039b29ef55239436f60fb9055d8bbc8d417f
antvinni <ant.vinni@gmail.com>
2024-09-08 13:48:32 +0100

    prettified code main, no logic changes

--- main.py
+++ main.py
@@ -7,55 +7,53 @@ from asteroidfield import AsteroidField
 from shot import Shot
 
 def main():
+    
+    #Initialise the game
     pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
-    # The center of the screen:
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
+    #setting the window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    #Groups
+    #Game groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
 
-    #add all Players and Asteroids to the groups
+    #Add game objects to the groups
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
     Shot.containers = (updatable, drawable, shots)
 
-    #create a player
-    player = Player(x, y, PLAYER_RADIUS)
-    asteroidField = AsteroidField()
-
-    #setting the window
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    #RGB colours
-    red = (255, 0, 0)
-    black = (0, 0, 0)
-
+    #Create a player and spawn it to the center of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     
-    clock = pygame.time.Clock() #clock to limit FPS
-    dt = 0  #delta time since last frame in seconds
+    #Create asteroid field
+    asteroidField = AsteroidField()
+    
+    #Game Clock
+    clock = pygame.time.Clock() 
+    dt = 0  #delta to store time since last frame in seconds
 
     ### GAME LOOP ###
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill(black) # fill the screen with black
         
+        #Screen background
+        screen.fill('black') 
+        
+        #Update game objects
         for obj in updatable:
             obj.update(dt)
         
+        #Game mechanics
         for asteroid in asteroids:
             if asteroid.is_collided(player):
+                
                 #Loosing lifes
                 player.num_lifes -= 1
                 print(f"You died. {player.num_lifes} life out of {PLAYER_NUM_LIFES} left")
@@ -69,7 +67,8 @@ def main():
                         print("Your score is recored in scores.txt")
                     print("Game over!")
                     sys.exit()
-
+            
+            #Shooting
             for shot in shots:
                 if asteroid.is_collided(shot): 
                     asteroid.split()
@@ -79,9 +78,11 @@ def main():
         for obj in drawable:
             obj.draw(screen) # render drawable on the scrren
         
+        #Update the display
         pygame.display.flip()
-
-        dt = clock.tick(60)/1000 #limit FPS to 60 and store time since last frame in dt in seconds
+        
+        #Game Timer. Limit frame rate to 60 FPS and store time since last frame in dt in seconds
+        dt = clock.tick(60)/1000 
 
 if __name__ == "__main__":
     main()
